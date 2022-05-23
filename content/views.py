539d3393cb612fbd7081b79d25ffe61a217a4557
LeeMinjea <gabe0529@naver.com>
2022-05-24 06:02:32 +0900

    make API to upload file to server

--- content/views.py
+++ content/views.py
@@ -1,6 +1,13 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
 # APIView import
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# uuid
+from uuid import uuid4
+from django_study.settings import MEDIA_ROOT
 
 from .models import Feed
 # Create your views here.
@@ -10,3 +17,25 @@ class Main(APIView):
         feed_list = Feed.objects.all().order_by('-id') # select * from feed
 
         return render(request, 'django_study/main.html', context=dict(feeds=feed_list))
+
+class UploadFeed(APIView):
+    def post(self, request):
+
+        file = request.FILES['file']
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+
+        image = uuid_name
+        content = request.data.get("content")
+        user_id = request.data.get("user_id")
+        profile_image = request.data.get("profile_image")
+        like_count = request.data.get("like_count")
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=like_count)
+
+        return Response(status=200)
--- django_study/urls.py
+++ django_study/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-from .views import Main_2
 from content.views import Main
+from content.views import UploadFeed
+from django.conf.urls.static import static
+from .settings import MEDIA_URL, MEDIA_ROOT 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
+    path("content/upload", UploadFeed.as_view()),
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
