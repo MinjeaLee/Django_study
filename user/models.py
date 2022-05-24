d5863e550364488f7e857437d89762bf2f801f26
LeeMinjea <gabe0529@naver.com>
2022-05-25 02:05:42 +0900

    join clone start

--- user/models.py
+++ user/models.py
@@ -1,3 +1,25 @@
+import email
 from django.db import models
+# AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    # profile_image
+    # user_id : nickname
+    # user_email
+    # user_password => default
+    # name
+
+    profile_image = models.TextField()
+    user_id = models.CharField(max_length=24, unique=True)
+    name = models.CharField(max_length=24)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'user_id'
+
+
+    class Meta:
+        db_table = 'user'
+
