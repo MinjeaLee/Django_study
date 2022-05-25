fe12f9b0cddc1e412e700325b903eab0dd2b7f4b
LeeMinjea <gabe0529@naver.com>
2022-05-26 01:38:21 +0900

    add profile

--- content/views.py
+++ content/views.py
@@ -4,19 +4,31 @@ from django.shortcuts import render
 # APIView import
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 # uuid
 from uuid import uuid4
 from django_study.settings import MEDIA_ROOT
-
 from .models import Feed
+
+from user.models import User
 # Create your views here.
 class Main(APIView):
     def get(self, request):
 
         feed_list = Feed.objects.all().order_by('-id') # select * from feed
 
-        return render(request, 'django_study/main.html', context=dict(feeds=feed_list))
+        print("로그인 한 사용자 정보: ", request.session.get('user'))
+
+        email = request.session.get('email', None)
+        user = User.objects.filter(email=email).first()
+
+        if email is None:
+            return render(request, 'user/login.html')
+        if user is None:
+            return render(request, 'user/login.html')
+
+
+
+        return render(request, 'django_study/main.html', context=dict(feeds=feed_list, user=user))
 
 class UploadFeed(APIView):
     def post(self, request):
@@ -29,7 +41,6 @@ class UploadFeed(APIView):
             for chunk in file.chunks():
                 destination.write(chunk)
 
-
         image = uuid_name
         content = request.data.get("content")
         user_id = request.data.get("user_id")
@@ -39,3 +50,7 @@ class UploadFeed(APIView):
         Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=like_count)
 
         return Response(status=200)
+
+class Profile(APIView):
+    def get(self, request):
+        return render(request, 'django_study/profile.html')
